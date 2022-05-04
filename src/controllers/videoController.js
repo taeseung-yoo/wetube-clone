6cc8f9d2f2cd78a5247a0be36d9f18f1cd8b73c3
taeseung-yoo <59465914+taeseung-yoo@users.noreply.github.com>
2022-05-04 22:07:29 +0900

    Added Regular Expression

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,16 +3,16 @@ export const recommendation = (req, res) => {
 };
 
 export const see = (req, res) => {
-  res.send("Watch Video");
+  return res.send(`Watch Video #${req.params.id}`);
 };
 export const edit = (req, res) => {
-  res.send("Edit Video");
+  return res.send(`Edit Video #${req.params.id}`);
 };
 export const search = (req, res) => {
   res.send("Search");
 };
 export const deleteVideo = (req, res) => {
-  res.send("Delete Video");
+  return res.send(`Delete Video #${req.params.id}`);
 };
 export const upload = (req, res) => {
   res.send("Upload video");
