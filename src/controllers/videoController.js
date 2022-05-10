09315f574075e8312b41727e7f20aa1587f288b9
taeseung-yoo <59465914+taeseung-yoo@users.noreply.github.com>
2022-05-10 20:57:07 +0900

    Handle Post Request

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,44 +1,46 @@
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createAt: "2 minutes ago",
+    views: 10,
+    id: 0,
+  },
+  {
+    title: "Second Video",
+    rating: 3,
+    comments: 7,
+    createAt: "29 minutes ago",
+    views: 159,
+    id: 1,
+  },
+  {
+    title: "Third Video",
+    rating: 4,
+    comments: 21,
+    createAt: "58 minutes ago",
+    views: 276,
+    id: 2,
+  },
+];
+
 export const recommendation = (req, res) => {
-  const videos = [
-    {
-      title: "First Video",
-      rating: 5,
-      comments: 2,
-      createAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Second Video",
-      rating: 3,
-      comments: 7,
-      createAt: "29 minutes ago",
-      views: 159,
-      id: 2,
-    },
-    {
-      title: "Third Video",
-      rating: 4,
-      comments: 21,
-      createAt: "58 minutes ago",
-      views: 276,
-      id: 3,
-    },
-  ];
   res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => {
-  res.render("watch", { pageTitle: "Watch" });
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id];
+  res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
 };
-export const edit = (req, res) => {
-  res.render("edit", { pageTitle: "Edit" });
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id];
+  res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
 };
-export const search = (req, res) => {
-  res.send("Search");
-};
-export const deleteVideo = (req, res) => {
-  return res.send(`Delete Video #${req.params.id}`);
-};
-export const upload = (req, res) => {
-  res.send("Upload video");
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id].title = title;
+  return res.redirect(`/videos/${id}`);
 };
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,12 +1,11 @@
 import express from "express";
-import { recommendation, search } from "../controllers/videoController";
+import { recommendation } from "../controllers/videoController";
 import { join, login } from "../controllers/userController";
 
 const globalRouter = express.Router();
 
 globalRouter.get("/", recommendation);
 globalRouter.get("/join", join);
-globalRouter.get("login", login);
-globalRouter.get("search", search);
+globalRouter.get("/login", login);
 
 export default globalRouter;
