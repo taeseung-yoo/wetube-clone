85bb6b076e1920969f6a75f64fdbddaf8244c24a
taeseung-yoo <59465914+taeseung-yoo@users.noreply.github.com>
2022-05-13 16:46:36 +0900

    Added MongoDB & Mongoose

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import "./db";
+import "./models/Video";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
@@ -18,6 +20,6 @@ app.use("/users", userRouter);
 app.use("/videos", videoRouter);
 
 const handleListening = () =>
-  console.log(`Server listening on port http://localhost:${PORT}`);
+  console.log(`✅ Server listening on port http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
