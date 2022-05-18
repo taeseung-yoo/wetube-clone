11212181269f5c3a2fa9053014f5aa8b4aab9b7d
taeseung-yoo <59465914+taeseung-yoo@users.noreply.github.com>
2022-05-19 05:15:36 +0900

    User Login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -56,6 +56,8 @@ export const postLogin = async (req, res) => {
       errorMessage: "Wrong Password",
     });
   }
+  req.session.loggedIn = true;
+  req.session.user = user;
   return res.redirect("/");
 };
 export const edit = (req, res) => {
